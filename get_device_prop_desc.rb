8e3309285b52f58d3548efd50b0c49cac9fc8af9
Toshiharu Takematsu <info@newfish.jp>
2014-09-02 23:50:32 +0900

    Refactoring: GetDevicePropDesc all-known device properties.

--- get_device_prop_desc.rb
+++ get_device_prop_desc.rb
@@ -1,43 +1,23 @@
 require './lib/theta_initiator.rb'
 
+def get_device_prop_desc initiator, device_property_name
+  puts "  #{device_property_name.to_s}[#{initiator.device_property_code(device_property_name).to_s(16)}]:"
+  begin
+    res = initiator.operation(:GetDevicePropDesc, [initiator.device_property_code(device_property_name)])
+    desc = PTP_DevicePropDesc.create res[:data]
+    puts "    #{desc.to_s}"
+  rescue => e
+    puts "    DevicePropNotSupported"
+  end
+end
+
 ThetaInitiator.open do |i|
 
   puts "DevicePropDesc:"
 
-  res = i.operation(:GetDevicePropDesc, [i.device_property_code(:BatteryLevel),0,0])
-  desc = PTP_DevicePropDesc.create res[:data]
-  puts "  BatteryLevel:\n    #{desc.to_s}"
-
-  res = i.operation(:GetDevicePropDesc, [i.device_property_code(:WhiteBalance),0,0])
-  desc = PTP_DevicePropDesc.create res[:data]
-  puts "  WhiteBalance:\n    #{desc.to_s}"
-
-  res = i.operation(:GetDevicePropDesc, [i.device_property_code(:DateTime),0,0])
-  desc = PTP_DevicePropDesc.create res[:data]
-  puts "  DateTime:\n    #{desc.to_s}"
-
-  begin
-    i.operation(:GetDevicePropDesc, [i.device_property_code(:ExposureTime),0,0])
-  rescue => e
-    puts "  ExposureTime:\n    #{e.to_s}"
+  PtpCode::DEVICE_PROPERTIES.each do |code,name|
+    next if name == 'Undefined'
+    get_device_prop_desc i, name
   end
-  
-  begin
-    i.operation(:GetDevicePropDesc, [i.device_property_code(:ExposureProgramMode),0,0])
-  rescue => e
-    puts "  ExposureProgramMode:\n    #{e.to_s}"
-  end
-  
-  res = i.operation(:GetDevicePropDesc, [i.device_property_code(:ExposureIndex),0,0])
-  desc = PTP_DevicePropDesc.create res[:data]
-  puts "  ExposureIndex:\n    #{desc.to_s}"
-  
-  res = i.operation(:GetDevicePropDesc, [i.device_property_code(:ExposureBiasCompensation),0,0])
-  desc = PTP_DevicePropDesc.create res[:data]
-  puts "  ExposureBiasCompensation:\n    #{desc.to_s}"
-
-  res = i.operation(:GetDevicePropDesc, [i.device_property_code(:StillCaptureMode),0,0])
-  desc = PTP_DevicePropDesc.create res[:data]
-  puts "  StillCaptureMode:\n    #{desc.to_s}"
   
 end
