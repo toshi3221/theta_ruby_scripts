c2801b4073d9e5a654520eb313bb6e973ab1c5f0
tomohiko112 <tm112uv@yahoo.co.jp>
2015-08-25 16:40:10 +0900

    Add capture for hdr

---
+++ capture_for_hdr.rb
@@ -0,0 +1,42 @@
+require './lib/theta_initiator.rb'
+
+ThetaInitiator.open do |initiator|
+
+	t = Time.now
+	date = "#{t.year}-#{t.month}-#{t.day}-#{t.hour}#{t.min}#{t.sec}"
+	Dir.mkdir_p("./outputs/HDR-#{date}")
+
+	BRIGHTNESS = [-2000,0,2000]
+
+	object_handles = Array.new(3) 
+
+	i = 0
+
+	for value in BRIGHTNESS
+		initiator.operation(:SetDevicePropValue, 
+			[0x5010],[value].pack('S').unpack('C*'))
+
+		response = initiator.operation(:GetDevicePropValue,
+			[0x5010])
+		puts "ExposureBiasCompensation: #{response[:data].pack('C*').unpack('S')[0].inspect}"
+
+		initiator.operation :InitiateCapture, [0,0]
+		response = initiator.wait_event #イベントが終わるまで待機
+		object_handles[i] = response[:parameters][0]
+
+		initiator.wait_event　#イベントが終わるまで待機
+		i+=1
+	end
+
+	i=0
+
+	for value in BRIGHTNESS
+		puts "GetObject...#{i}/3"
+		response = initiator.operation(:GetObject, [object_handles[i]])
+		File.open("./outputs/HDR-#{date}/theta_pic_#{value}.jpg", "wb") do |f|
+			f.write(response[:data].pack("C*"))
+			puts "#{i}/3 Saved!"
+		end
+		i+=1
+	end
+end
