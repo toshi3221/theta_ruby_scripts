253ef169be0bb3bf3f45dd60eb6c6f8842bf7b75
tomohiko112 <tm112uv@yahoo.co.jp>
2015-08-26 09:08:11 +0900

    Fixed some points

--- capture_for_hdr.rb
+++ capture_for_hdr.rb
@@ -1,10 +1,11 @@
 require './lib/theta_initiator.rb'
+require 'FileUtils'
 
 ThetaInitiator.open do |initiator|
 
 	t = Time.now
 	date = "#{t.year}-#{t.month}-#{t.day}-#{t.hour}#{t.min}#{t.sec}"
-	Dir.mkdir_p("./outputs/HDR-#{date}")
+	FileUtils.mkdir_p("./outputs/HDR-#{date}")
 
 	BRIGHTNESS = [-2000,0,2000]
 
@@ -18,24 +19,24 @@ ThetaInitiator.open do |initiator|
 
 		response = initiator.operation(:GetDevicePropValue,
 			[0x5010])
-		puts "ExposureBiasCompensation: #{response[:data].pack('C*').unpack('S')[0].inspect}"
+		puts "ExposureBiasCompensation: #{response[:data].pack('C*').unpack('s')[0].inspect}"
 
 		initiator.operation :InitiateCapture, [0,0]
 		response = initiator.wait_event #イベントが終わるまで待機
 		object_handles[i] = response[:parameters][0]
 
-		initiator.wait_event　#イベントが終わるまで待機
+		initiator.wait_event #イベントが終わるまで待機
 		i+=1
 	end
 
 	i=0
 
 	for value in BRIGHTNESS
-		puts "GetObject...#{i}/3"
+		puts "GetObject...#{i+1}/3"
 		response = initiator.operation(:GetObject, [object_handles[i]])
 		File.open("./outputs/HDR-#{date}/theta_pic_#{value}.jpg", "wb") do |f|
 			f.write(response[:data].pack("C*"))
-			puts "#{i}/3 Saved!"
+			puts "#{i+1}/3 Saved!"
 		end
 		i+=1
 	end
