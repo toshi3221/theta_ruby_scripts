755d1fcd54aa0f9a1d07cc7a133319192de16f19
tomohiko112 <tm112uv@yahoo.co.jp>
2015-08-27 10:31:41 +0900

    Fixed some points & Added show data size.

--- capture_for_hdr.rb
+++ capture_for_hdr.rb
@@ -1,16 +1,16 @@
 require './lib/theta_initiator.rb'
 require 'FileUtils'
-
+require 'open3'
 
 ThetaInitiator.open do |initiator|
 
-	Current = Dir.pwd
+	current = Dir.pwd
 
 	t = Time.now
 	date = "#{t.year}-#{t.month}-#{t.day}-#{t.hour}#{t.min}#{t.sec}"
 	FileUtils.mkdir_p("./outputs/HDR-#{date}")
 
-	BRIGHTNESS = [-1000,0,1000]
+	BRIGHTNESS = [-1300,0,1300]
 
 	object_handles = Array.new(3)
 	file_path = Array.new(3)   
@@ -19,33 +19,38 @@ ThetaInitiator.open do |initiator|
 
 	for value in BRIGHTNESS
 		initiator.operation(:SetDevicePropValue, 
-			[0x5010],[value].pack('S').unpack('C*'))
+			[initiator.device_property_code(:ExposureBiasCompensation)],[value].pack('S').unpack('C*'))
 
 		response = initiator.operation(:GetDevicePropValue,
-			[0x5010])
+			[initiator.device_property_code(:ExposureBiasCompensation)])
 		puts "ExposureBiasCompensation: #{response[:data].pack('C*').unpack('s')[0].inspect}"
 
+		
 		initiator.operation :InitiateCapture, [0,0]
-		response = initiator.wait_event #イベントが終わるまで待機
+		response = initiator.wait_event #InitiateCaptureが終わるまで待機
+		
 		object_handles[i] = response[:parameters][0]
-
-		initiator.wait_event #イベントが終わるまで待機
+		initiator.wait_event #object_handles=respose[:parameters][0]が終わるまで待機
+		
 		i+=1
 	end
 
 	i=0
 
 	for value in BRIGHTNESS
+		a=0
 		puts "GetObject...#{i+1}/3"
-		response = initiator.operation(:GetObject, [object_handles[i]])
-		File.open("./outputs/HDR-#{date}/theta_pic_#{value}.jpg", "wb") do |f|
-			f.write(response[:data].pack("C*"))
-			puts "#{i+1}/3 Saved!"
-			file_path[i] = "/outputs/HDR-#{date}/theta_pic_#{value}.jpg"
+		file_path[i] = "/outputs/HDR-#{date}/theta_pic_#{value}.jpg"
+		data_size = File.open(".#{file_path[i]}", "wb") do |f|
+			response = initiator.operation(:GetObject, [object_handles[i]]) do |data|
+				f.write data
+			end
+			puts "#{i+1}/3 Saved (data_size : #{response[:data_size]} byte)"
 		end
 		i+=1
 	end
 
-	system "luminance-hdr-cli.exe -s #{Current}/outputs/HDR-#{date}/HDR.jpg #{Current}#{file_path[1]} #{Current}#{file_path[0]} #{Current}#{file_path[2]}"
-	system "luminance-hdr-cli.exe -o #{Current}/outputs/HDR-#{date}/LDR.jpg #{Current}#{file_path[1]} #{Current}#{file_path[0]} #{Current}#{file_path[2]}"
+	#Open3.capture 出力抑制のため
+	Open3.capture3 ("luminance-hdr-cli.exe --tmo mantiuk08 -o #{current}/outputs/HDR-#{date}/HDR.jpg #{current}#{file_path[1]} #{current}#{file_path[0]} #{current}#{file_path[2]}")
+
 end
